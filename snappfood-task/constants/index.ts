eb2ade877c876bdf024947b7812c6bda04fb3f9e
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-08-13 16:32:46 +0430

    [feat] add vendor types and contant data

---
+++ snappfood-task/constants/index.ts
@@ -0,0 +1,24 @@
+import { Vendor } from '../types';
+
+export const vendors: Vendor[] = [
+  {
+    deliveryType: 'در لحظه و پر قدرت :)',
+    CoverImage: 'https://snappfood.ir/static/images/vendor_pic.png',
+    description: '_دیسکریپشن_',
+    id: '2',
+    price: '100',
+    restaurantRate: '4.5',
+    restaurantTitle: '_دیسکریپشن_',
+    logo: 'https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_1_603508bf202d8_img_st_food.png',
+  },
+  {
+    deliveryType: 'در لحظه و پر قدرت :)',
+    CoverImage: 'https://snappfood.ir/static/images/vendor_pic.png',
+    description: '_دیسکریپشن_',
+    id: '2',
+    price: '100',
+    restaurantRate: '4.5',
+    restaurantTitle: '_دیسکریپشن_',
+    logo: 'https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_1_603508bf202d8_img_st_food.png',
+  },
+];
---
+++ snappfood-task/types/vendor.ts
@@ -0,0 +1,10 @@
+export type Vendor = {
+  id: string;
+  CoverImage: string;
+  logo: string;
+  restaurantTitle: string;
+  restaurantRate: string;
+  price: string;
+  deliveryType: string;
+  description: string;
+};
