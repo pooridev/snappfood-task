1f54174b9c01dee2c3fb2df41c2af36f40863f16
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-08-14 23:34:05 +0430

    [update] vendors  normalizor

--- snappfood-task/config/normalizers/vendors.ts
+++ snappfood-task/config/normalizers/vendors.ts
@@ -1,11 +1,24 @@
 import { Vendor } from './../../types/vendor';
 
-export const normalizeVendors = (vendors: Vendor[]) => {
-  const vendorsWithCorrectData = vendors.filter((vendor: any) => {
-    if (typeof vendor.data === 'object') {
-      return vendor.data;
-    }
-  });
+export const normalizeVendors = (vendors: { data: Vendor; type: string }[]) => {
+  debugger;
+  const vendorsWithCorrectData = vendors
+    // Remove uncorrect data which is not object
+    .filter((vendor) => {
+      if (typeof vendor.data === 'object') {
+        return vendor.data;
+      }
+    })
+    // set default data for each vendor
+    .map((vendor) => {
+      return {
+        ...vendor.data,
+        title: vendor.data.title ?? 'بون سی',
+        deliveryFee: vendor.data.deliveryFee ?? 2500,
+      };
+    })
+    // remove undefined data
+    .filter(Boolean);
 
   return vendorsWithCorrectData;
 };
