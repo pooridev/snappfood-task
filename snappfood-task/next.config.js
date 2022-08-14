eac7f8ad2922f270b7252dd14879ec1bf9cf8795
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-08-14 23:41:32 +0430

    [feat] enable top level await in webpack config

--- snappfood-task/next.config.js
+++ snappfood-task/next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
   },
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true
+    return config;
+  },
 };
 
 module.exports = nextConfig;
