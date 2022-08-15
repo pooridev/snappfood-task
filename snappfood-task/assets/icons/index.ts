346176a255d3b54b7f66db929c542f42d1bc3989
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-08-16 02:45:22 +0430

    [update] export icons in index file

--- snappfood-task/assets/icons/index.ts
+++ snappfood-task/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as ExpressIcon } from './ExpressIcon';
 export { default as VendorIcon } from './VendorIcon';
 export { default as StarIcon } from './StarIcon';
+export { default as DeliveryIcon } from './DeliveryIcon';
