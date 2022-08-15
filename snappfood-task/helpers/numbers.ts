4ef283df2aff58985529a9d51743ea49e3b2e06d
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-08-16 02:54:48 +0430

    [feat] add toPersianDigit helper

--- snappfood-task/helpers/numbers.ts
+++ snappfood-task/helpers/numbers.ts
@@ -2,3 +2,9 @@ export const formatPrice = (number: number): string => {
   const intl = new Intl.NumberFormat('fa-IR');
   return intl.format(number);
 };
+
+const persianNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+export const toPersianDigit = (text: string | number): string => {
+  return text.toString().replace(/[0-9]/g, (w) => persianNumbers[+w]);
+};
