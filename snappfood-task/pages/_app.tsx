c71bb72a5bcd118bb4da2be1cdfee122907bb752
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-08-16 02:43:06 +0430

    [fix] provide app

--- snappfood-task/pages/_app.tsx
+++ snappfood-task/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import 'react-virtualized/styles.css';
+
+import '../styles/main.scss';
+import store from '../store';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
